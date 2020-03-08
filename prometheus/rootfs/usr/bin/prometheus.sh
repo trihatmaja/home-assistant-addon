c649136149148077ea90261b2e6adedec5e676b5
Wicaksono Trihatmaja <trihatmaja@gmail.com>
2020-03-08 20:13:53 +0700

    fix config

--- prometheus/rootfs/usr/bin/prometheus.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Home Assistant Add-on: Prometheus
-#
-# Prometheus add-on for Hass.io.
-# This addon to start the prometheus server.
-# ==============================================================================
