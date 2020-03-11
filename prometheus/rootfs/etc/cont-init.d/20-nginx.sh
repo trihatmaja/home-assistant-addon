03a742546ee29a8e8e1a65f3cf2ba5cc1609f97d
Wicaksono Trihatmaja <trihatmaja@gmail.com>
2020-03-11 13:21:39 +0700

    fixing

--- prometheus/rootfs/etc/cont-init.d/20-nginx.sh
+++ prometheus/rootfs/etc/cont-init.d/20-nginx.sh
@@ -4,13 +4,6 @@
 # Configures NGINX for use with ttyd
 # ==============================================================================
 declare dns_host
-declare ingress_interface
-declare ingress_port
-
-ingress_port=$(bashio::addon.ingress_port)
-ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/nginx.conf
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/nginx.conf
 
 dns_host=$(bashio::dns.host)
 sed -i "s/%%dns_host%%/${dns_host}/g" /etc/nginx/nginx.conf
