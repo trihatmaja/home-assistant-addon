ecbde491f42c5b4a234f049c0053cb7724b9cf69
Wicaksono Trihatmaja <trihatmaja@gmail.com>
2020-03-13 06:33:30 +0700

    fixing nginx

--- prometheus/rootfs/etc/cont-init.d/20-nginx.sh
+++ prometheus/rootfs/etc/cont-init.d/20-nginx.sh
@@ -3,34 +3,14 @@
 # Home Assistant Community Add-on: Prometheus
 # Configures NGINX for use with ttyd
 # ==============================================================================
-declare port
-declare certfile
 declare dns_host
 declare ingress_entry
 declare ingress_interface
 declare ingress_port
-declare keyfile
 
-
-port=$(bashio::addon.port 80)
-if bashio::var.has_value "${port}"; then
-    bashio::config.require.ssl
-
-    if bashio::config.true 'ssl'; then
-        certfile=$(bashio::config 'certfile')
-        keyfile=$(bashio::config 'keyfile')
-
-        mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
-        sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/servers/direct.conf
-        sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/servers/direct.conf
-
-    else
-        mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
-    fi
-
-    ingress_entry=$(bashio::addon.ingress_entry)
-    sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/direct.conf
-fi
+mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
+ingress_entry=$(bashio::addon.ingress_entry)
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/direct.conf
 
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
